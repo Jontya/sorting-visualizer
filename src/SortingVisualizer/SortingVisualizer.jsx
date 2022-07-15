3f1127fa47a07c7447f1ff7fd9b2865463ef4dcd
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-07-15 20:36:53 +1000

    Added Merge Sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import {mergeSortAnimations} from "../sortingAlgorithms/sortingAlgorithms.js";
 import "./SortingVisualizer.css";
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 240;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = "#C0C0C0";
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
 export default class SortingVisualizer extends React.Component{
     constructor(props){
@@ -16,51 +29,67 @@ export default class SortingVisualizer extends React.Component{
 
     resetArray(){
         const array = [];
-        for(let i = 0; i < 240; i++){
+        for(let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
             array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
 
     mergeSort(){
-
+        const animations = mergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
     }
 
     quickSort(){
-
+        
     }
 
     heapSort(){
-
+        
     }
 
     bubbleSort(){
-
+        
     }
 
     render(){
         const {array} = this.state;
-        const max = Math.max(...array);
-        console.log(max);
         return(
             <div className="project-container">
-                <div className="parent-container">
-                    <div className="array-container" class="centre">
-                        {array.map((value, idx) => (
-                            <div 
-                                className="array-bar" 
-                                key ={idx} 
-                                style={{height: `${value/9.5}vh`, marginTop: `${730-Math.max(...array)}px`}}>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="button-container">
+                <div className="array-container">
+                    {array.map((value, idx) => (
+                        <div 
+                            className="array-bar" 
+                            key ={idx} 
+                            style={{height: `${value/9.5}vh`, marginTop: `${730-Math.max(...array)}px`}}>
+                        </div>
+                    ))}
+                </div>
+                <div className="button-container">
                         <button on onClick={() => this.resetArray()}>Generate New Array</button>
                         <button onClick={() => this.mergeSort()}>Merge Sort</button>
                         <button onClick={() => this.quickSort()}>Quick Sort</button>
                         <button onClick={() => this.heapSort()}>Heap Sort</button>
                         <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-                    </div>
                 </div>
             </div>
             
@@ -70,5 +99,17 @@ export default class SortingVisualizer extends React.Component{
 
 function randomIntFromInterval(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
